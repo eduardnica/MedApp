c6f141fe186c425a8ae260a90baee9650600ac23
eduardnica <71974795+eduardnica@users.noreply.github.com>
2022-05-29 14:45:01 +0300

    added registerActivity

---
+++ app/src/main/java/csie/aplicatielicenta/LoginActivity.java
@@ -0,0 +1,52 @@
+package csie.aplicatielicenta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+        TextView email = findViewById(R.id.email);
+        TextView password = findViewById(R.id.password);
+        TextView register = findViewById(R.id.register);
+
+        MaterialButton btnLogin = (MaterialButton) findViewById(R.id.btnLogin);
+
+        //admin and admin
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct
+                    Toast.makeText(LoginActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+
+                } else
+                    //incorrect
+                    Toast.makeText(LoginActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
